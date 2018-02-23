93b28cd97af900fee2225b93097447e9937b8cad
nikhilpooja <nik703.v@gmail.com>
2018-02-23 19:19:09 +0530

    add file

--- bin/HTTPServlet/src/com/briglabz/httpservlet/EmpUpdateDelete.java
+++ bin/HTTPServlet/src/com/briglabz/httpservlet/EmpUpdateDelete.java
@@ -6,7 +6,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
---
+++ bin/HibernateDemo/src/com/bridgelabz/hibernate/Employee.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.hibernate;
+
+public class Employee {
+	
+	private int Id;
+	private String firstName, lastName;
+	
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+
+}
